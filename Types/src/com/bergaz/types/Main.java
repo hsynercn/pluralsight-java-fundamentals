94326a7db4c36ba2e6b0dac0b3b89604a95bef33
hsyn <huscanercan@gmail.com>
2020-06-29 22:53:28 +0300

    type examples

---
+++ Types/src/com/bergaz/types/Main.java
@@ -0,0 +1,24 @@
+package com.bergaz.types;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final int a = 0;
+        final int b;
+        b = a;
+        String myVar = null;
+
+        byte x = 8;
+        short y = 16;
+        int z = 32;
+        long t = 98999999L;
+        float g = 0.00001f;
+        double h = 0.0000000000000000007;
+        char o = '\u00DD';
+
+        int first = 5;
+        int second = 5;
+        System.out.println(++first);
+        System.out.println(second++);
+    }
+}
