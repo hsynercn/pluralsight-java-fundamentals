f0b2739c165596c573e7337efe7532ced2ba4d09
hsyn <huscanercan@gmail.com>
2020-07-05 12:25:50 +0300

    String compare, formatting

--- Types/src/com/bergaz/types/CalcEngine.java
+++ Types/src/com/bergaz/types/CalcEngine.java
@@ -1,5 +1,8 @@
 package com.bergaz.types;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class CalcEngine {
     public static void main(String[] args) {
         double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
@@ -14,12 +17,48 @@ public class CalcEngine {
             for(double temp : results) {
                 System.out.println(temp);
             }
+        } else if(args.length == 1 && args[0].equals("interactive")) {
+            executeInteractively();
         } else if(args.length == 3){
             handleCommandLine(args);
         } else {
             System.out.println("Invalid input");
         }
+    }
 
+    static void executeInteractively() {
+        System.out.println("Enter an operation and two numbers:");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] parts = userInput.split(" ");
+        performOperation(parts);
+    }
+
+    private static void performOperation(String[] parts) {
+        char opCode = opCodeFromString(parts[0]);
+        double leftVal = valueFromWord(parts[1]);
+        double rightVal = valueFromWord(parts[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        displayResult(opCode, leftVal, rightVal, result);
+    }
+
+    private static char symbolFromOpCode(char opCode) {
+        HashMap<Character, Character> opMap = new HashMap<>();
+        opMap.put('a', '+');
+        opMap.put('s', '-');
+        opMap.put('m', '*');
+        opMap.put('d', '/');
+        return opMap.get(opCode);
+    }
+
+    private static void displayResult(char opCode, double leftVal, double rightVal, double result) {
+        char symbol = symbolFromOpCode(opCode);
+        //StringBuilder builder = new StringBuilder(20);
+        //builder.append(leftVal).append(" ").append(symbol);
+        //builder.append(" ").append(rightVal).append( " = ");
+        //builder.append(result);
+        //System.out.println(builder.toString());
+        String output = String.format("%.2f %c %.2f = %.2f",leftVal, opCode, rightVal, result);
     }
 
     private static void handleCommandLine(String[] args) {
@@ -50,5 +89,26 @@ public class CalcEngine {
                 result = 0.0d;
         }
         return result;
+    }
+
+    static char opCodeFromString(String operationName) {
+        char opCode = operationName.charAt(0);
+        return opCode;
+    }
+
+    static double valueFromWord(String word) {
+        String[] numberWords = {
+                "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"
+        };
+        double value = 0d;
+        for(String temp : numberWords) {
+            if(temp.equals(word)) {
+                return value;
+            } else {
+                value += 1.0d;
+            }
+        }
+        return value;
     }
 }
