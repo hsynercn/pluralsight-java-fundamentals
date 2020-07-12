204c6d3e3acc70e5fe004614b23417c5ea33c33d
hsyn <huscanercan@gmail.com>
2020-07-12 23:45:03 +0300

    sorted set

--- Types/src/com/bergaz/collections/ProductCatalogue.java
+++ Types/src/com/bergaz/collections/ProductCatalogue.java
@@ -1,11 +1,10 @@
 package com.bergaz.collections;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class ProductCatalogue implements Iterable<Product> {
+
+    private final SortedSet<Product> productSortedSet = new TreeSet<>(Product.BY_WEIGHT);
 
     private final Set<Product> products = new HashSet<>();
 
@@ -17,5 +16,24 @@ public class ProductCatalogue implements Iterable<Product> {
     @Override
     public Iterator<Product> iterator() {
         return null;
+    }
+
+    public Set<Product> lightVanProducts() {
+        Product heaviestLightVanProduct = findHeaviestLightVanProduct();
+        return productSortedSet.headSet(heaviestLightVanProduct);
+    }
+
+    private Product findHeaviestLightVanProduct() {
+        for(Product product : productSortedSet) {
+            if(product.getWeight() > 20) {
+                return product;
+            }
+        }
+        return productSortedSet.last();
+    }
+
+    public Set<Product> heavyVanProducts() {
+        Product heaviestLightVanProduct = findHeaviestLightVanProduct();
+        return productSortedSet.tailSet(heaviestLightVanProduct);
     }
 }
