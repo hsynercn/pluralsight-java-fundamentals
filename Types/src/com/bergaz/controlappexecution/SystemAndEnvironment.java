2355aa51c302543cb970e9ce4e0169e423d2f8e2
hsyn <huscanercan@gmail.com>
2020-07-24 10:44:31 +0300

    system and environment properties

---
+++ Types/src/com/bergaz/controlappexecution/SystemAndEnvironment.java
@@ -0,0 +1,15 @@
+package com.bergaz.controlappexecution;
+
+import java.util.Map;
+
+public class SystemAndEnvironment {
+    public static void main(String[] args) {
+        String userName = System.getProperty("user.name");
+        String userHome = System.getProperty("user.home");
+        String osArchitecture = System.getProperty("os.arch");
+        String javaVendor = System.getProperty("java.vendor");
+
+        Map<String, String> getenv = System.getenv();
+        getenv.forEach((k,v) -> System.out.println(k+">"+v));
+    }
+}
