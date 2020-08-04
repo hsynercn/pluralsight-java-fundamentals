156afa4a6772098913b3535648acc2f2e40e0a8d
hsyn <huscanercan@gmail.com>
2020-08-04 23:37:41 +0300

    CompletableFuture examples

---
+++ Types/src/com/bergaz/fundamentals/asynchronous/FirstCompletableFutures.java
@@ -0,0 +1,40 @@
+package com.bergaz.fundamentals.asynchronous;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FirstCompletableFutures {
+    public static void main(String[] args) throws InterruptedException {
+
+        //runRunnable();
+        runRunnableWithExecutor();
+
+    }
+
+    private static void runRunnableWithExecutor() throws InterruptedException {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Runnable task = () -> System.out.println("I am running asynchronously Thread: " + Thread.currentThread().getName() );
+        //This thread si from pool-1-thread-1
+        CompletableFuture.runAsync(task, executor);
+        /**
+         *  With providing an ExecutorService to runAsync we can prevent main thread from exiting. But in this case we
+         *  need to shutdown the executor manually, otherwise main thread can't exit. Shutdown executes all submitted
+         *  tasks.
+         */
+        executor.shutdown();
+    }
+
+    private static void runRunnable() throws InterruptedException {
+        Runnable task = () -> System.out.println("I am running asynchronously Thread: " + Thread.currentThread().getName());
+        //this thread is from ForkJoinPool
+        CompletableFuture.runAsync(task);
+        /**
+         *  Without sleep we can't see print, because upper task launched in common fork/joined pool. This fork/joined
+         *  pool made of deamon threads. They do not prevent from JVM exiting, thus upper task can't get a change to
+         *  print text.
+         */
+        Thread.sleep(100);
+    }
+}
---
+++ Types/src/com/bergaz/fundamentals/asynchronous/MyChainExample.java
@@ -0,0 +1,24 @@
+package com.bergaz.fundamentals.asynchronous;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class MyChainExample {
+    public static void main(String[] args) {
+        CompletableFuture cf = CompletableFuture.supplyAsync(() -> List.of(1L, 2L, 3L))
+                .thenApply(list ->{
+                    System.out.println(list);
+                    return list;
+                });
+
+        cf.thenRun(() -> System.out.println("The list of users has been read"));
+        boolean test = false;
+        cf.thenAccept(myVal -> System.out.println(myVal));
+
+        /**
+         * RUNNABLE method: void run(), cf method:thenRun()
+         * CONSUMER method: void accept(Long), cf method: thenAccept(); can take an object
+         * FUNCTION method: User apply(Long), cf thenApply(); can take object and can return object
+         */
+    }
+}
