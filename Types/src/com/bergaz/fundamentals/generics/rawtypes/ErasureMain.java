3a6acd5f2176f6c62a9ac73e0ee98d15cc424de3
hsynercn <huscanercan@gmail.com>
2020-08-20 15:19:45 +0300

    Erasure introduction sample

---
+++ Types/src/com/bergaz/fundamentals/generics/rawtypes/ErasureMain.java
@@ -0,0 +1,11 @@
+package com.bergaz.fundamentals.generics.rawtypes;
+
+public class ErasureMain {
+    public static void main(String[] args) {
+        /**
+         * Translation at compile time, not runtime
+         * Generic representation is a bit of a lie it only exists in source code
+         * After compilation we can see raw types are in use
+         */
+    }
+}
