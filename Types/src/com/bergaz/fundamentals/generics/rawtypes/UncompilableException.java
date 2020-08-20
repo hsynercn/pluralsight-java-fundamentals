8f430f1d80966373010b50057a0fe66ca2d9f3bc
hsynercn <huscanercan@gmail.com>
2020-08-20 15:57:23 +0300

    Implications of Erasure

---
+++ Types/src/com/bergaz/fundamentals/generics/rawtypes/UncompilableException.java
@@ -0,0 +1,16 @@
+package com.bergaz.fundamentals.generics.rawtypes;
+
+/**
+ * If we try to use T we get
+ * Generic class may not extend 'java.lang.Throwable'
+ * compile error
+ */
+public class UncompilableException/*<T>*/ extends Exception{
+    public static void main(String[] args) {
+        try {
+            throw new UncompilableException();
+        } catch (UncompilableException e) {
+            e.printStackTrace();
+        }
+    }
+}
