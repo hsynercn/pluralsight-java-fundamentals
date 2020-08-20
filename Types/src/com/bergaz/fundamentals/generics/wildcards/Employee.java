46e28c3e7796d754c3d4b094ae78e4017ffcd58c
hsynercn <huscanercan@gmail.com>
2020-08-20 11:06:02 +0300

    Upper bounded wildcard usage

---
+++ Types/src/com/bergaz/fundamentals/generics/wildcards/Employee.java
@@ -0,0 +1,12 @@
+package com.bergaz.fundamentals.generics.wildcards;
+
+public class Employee extends Person{
+    public Employee(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{name='" + getName() + "', age=" + getAge() + "}";
+    }
+}
---
+++ Types/src/com/bergaz/fundamentals/generics/wildcards/Main.java
@@ -0,0 +1,63 @@
+package com.bergaz.fundamentals.generics.wildcards;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * GENERICS IS ABOUT SAFETY WITHOUT CODE DUPLICATION
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File file;
+        PersonSaver saver = new PersonSaver(new File("mock_file"));
+        PersonLoader loader;
+
+        Partner donDraper = new Partner("Don Draper", 89);
+        Partner peggyDraper = new Partner("Peggy Olson", 65);
+        Employee bertCooper = new Employee("Bert Cooper", 100);
+
+        Person person = new Person("Bob", 20);
+        saver.save(person);
+
+        saver.savePersonType(donDraper);
+        saver.save(peggyDraper);
+        saver.save(bertCooper);
+
+        Person personType = donDraper;
+
+
+        List<Partner> partners = new ArrayList<>();
+        partners.add(donDraper);
+        partners.add(peggyDraper);
+        saver.saveAllPersonType(partners);
+
+        List<Person> persons = new ArrayList<>();
+        persons.add(donDraper);
+        persons.add(bertCooper);
+        saver.saveAll(persons);
+
+        /*
+        This is safe usage
+         */
+        Partner[] partnerArray = new Partner[2];
+        Person[] personArray = partnerArray;
+        partnerArray[0] = donDraper;
+        partnerArray[1] = peggyDraper;
+        saver.saveAll(partnerArray);
+
+        /*
+        This is unsafe usage, we assigned an Employee to a Partner array
+         */
+        Partner[] partnerArray1 = new Partner[3];
+        Person[] personArray1 = partnerArray1;
+        personArray1[0] = donDraper;
+        personArray1[1] = peggyDraper;
+        //personArray1[2] = bertCooper;
+
+        saver.saveAll(partnerArray);
+
+
+    }
+}
