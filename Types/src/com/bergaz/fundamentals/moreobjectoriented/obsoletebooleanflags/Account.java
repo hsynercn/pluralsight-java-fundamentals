ecf61ea4b94c63dd14f608bab9c4d058c8195537
hsynercn <huscanercan@gmail.com>
2020-08-30 10:50:55 +0300

    OOB branching complexity

---
+++ Types/src/com/bergaz/fundamentals/moreobjectoriented/obsoletebooleanflags/Account.java
@@ -0,0 +1,58 @@
+package com.bergaz.fundamentals.moreobjectoriented.obsoletebooleanflags;
+
+import java.math.BigDecimal;
+
+public class Account {
+
+    /**
+     * Step by step complexity is leaking in our class
+     * With every change out test case set is multiplying
+     */
+    private boolean isVerified;
+    private boolean isClosed;
+    private boolean isFrozen;
+    private BigDecimal balance;
+
+    public Account() {
+        this.balance = BigDecimal.ZERO;
+    }
+
+    public void helderVerified() {
+        this.isVerified = true;
+    }
+
+    public void closeAccount() {
+        this.isClosed = true;
+    }
+
+    public void freezeAccaount() {
+        if (!this.isVerified) {
+            return;
+        }
+        if (this.isClosed) {
+            return;
+        }
+        this.isFrozen = true;
+    }
+
+    public void deposit(BigDecimal amount) {
+        if (this.isClosed) {
+            return;
+        }
+        //Deposit money
+        if (this.isFrozen) {
+            this.isFrozen = false;
+        }
+        this.balance = this.balance.add(amount);
+    }
+    public void withdraw(BigDecimal amount) {
+        if (!this.isVerified) {
+            return;
+        }
+        if (this.isClosed) {
+            return;
+        }
+        //Withdraw money
+        this.balance = this.balance.subtract(amount);
+    }
+}
