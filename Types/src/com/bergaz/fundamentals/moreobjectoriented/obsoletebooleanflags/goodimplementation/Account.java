16bb189284018647f9807bcbe7291bc36a3f2778
hsynercn <huscanercan@gmail.com>
2020-08-30 15:43:27 +0300

    OOP branching completely shifted to State object

--- Types/src/com/bergaz/fundamentals/moreobjectoriented/obsoletebooleanflags/goodimplementation/Account.java
+++ Types/src/com/bergaz/fundamentals/moreobjectoriented/obsoletebooleanflags/goodimplementation/Account.java
@@ -1,59 +1,46 @@
 package com.bergaz.fundamentals.moreobjectoriented.obsoletebooleanflags.goodimplementation;
 
+import com.bergaz.fundamentals.moreobjectoriented.obsoletebooleanflags.goodimplementation.accountstates.AccountState;
+import com.bergaz.fundamentals.moreobjectoriented.obsoletebooleanflags.goodimplementation.accountstates.Active;
+
 import java.math.BigDecimal;
 
 public class Account {
 
-    /**
-     * Step by step complexity is leaking in our class
-     * With every change out test case set is multiplying
-     */
-    private boolean isVerified;
-    private boolean isClosed;
     private BigDecimal balance;
-    private Freezable freezable;
-
+    private AccountState state;
 
     public Account(AccountUnfrozen accountUnfrozen) {
         this.balance = BigDecimal.ZERO;
-        this.freezable = new FreezableActive(accountUnfrozen);
+        this.state = new Active(accountUnfrozen);
     }
 
-    public void helderVerified() {
-        this.isVerified = true;
+    public void holderVerified() {
+        this.state = this.state.holderVerified();
     }
 
     public void closeAccount() {
-        this.isClosed = true;
+        this.state = this.state.closeAccount();
     }
 
     public void freezeAccount() {
-        if (!this.isVerified) {
-            return;
-        }
-        if (this.isClosed) {
-            return;
-        }
-        this.freezable = this.freezable.freezeAccount();
+        this.state = this.state.freezeAccount();
     }
 
     public void deposit(BigDecimal amount) {
-        if (this.isClosed) {
-            return;
-        }
-        //Deposit money
-        this.freezable = this.freezable.deposit();
+        this.state = this.state.deposit(amount, this::addToBalance);
+    }
+
+    private void addToBalance(BigDecimal amount) {
         this.balance = this.balance.add(amount);
     }
-    public void withdraw(BigDecimal amount) {
-        if (!this.isVerified) {
-            return;
-        }
-        if (this.isClosed) {
-            return;
-        }
-        this.freezable = this.freezable.withdraw();
+
+    private void subtractFromBalance(BigDecimal amount) {
         this.balance = this.balance.subtract(amount);
+    }
+
+    public void withdraw(BigDecimal amount) {
+        this.state = this.state.withdraw(this.balance, amount, this::subtractFromBalance);
     }
 
 }
--- Types/src/com/bergaz/fundamentals/moreobjectoriented/obsoletebooleanflags/goodimplementation/Freezable.java
+++
@@ -1,7 +0,0 @@
-package com.bergaz.fundamentals.moreobjectoriented.obsoletebooleanflags.goodimplementation;
-
-public interface Freezable {
-    Freezable deposit();
-    Freezable withdraw();
-    Freezable freezeAccount();
-}
