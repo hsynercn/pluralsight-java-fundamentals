b78e5c58859f37d776bddbda54aae78e7c412de1
hsynercn <huscanercan@gmail.com>
2020-09-07 23:23:19 +0300

    OOP add special case object

---
+++ Types/src/com/bergaz/fundamentals/moreobjectoriented/specialcaseobject/LifetimeWarranty.java
@@ -0,0 +1,16 @@
+package com.bergaz.fundamentals.moreobjectoriented.specialcaseobject;
+
+import java.time.LocalDate;
+
+public class LifetimeWarranty implements Warranty{
+    private LocalDate issuedOn;
+
+    public LifetimeWarranty(LocalDate issuedOn) {
+        this.issuedOn = issuedOn;
+    }
+
+    @Override
+    public boolean isValidOd(LocalDate date) {
+        return this.issuedOn.compareTo(date) <= 0;
+    }
+}
