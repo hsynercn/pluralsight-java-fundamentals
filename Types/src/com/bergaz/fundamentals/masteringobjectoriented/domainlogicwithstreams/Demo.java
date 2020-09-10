0e20757b78588a34c9ff91e3db161e19355a4559
hsynercn <huscanercan@gmail.com>
2020-09-10 22:33:45 +0300

    Stream examples

--- Types/src/com/bergaz/fundamentals/masteringobjectoriented/domainlogicwithstreams/Demo.java
+++ Types/src/com/bergaz/fundamentals/masteringobjectoriented/domainlogicwithstreams/Demo.java
@@ -1,5 +1,6 @@
 package com.bergaz.fundamentals.masteringobjectoriented.domainlogicwithstreams;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Demo {
@@ -16,6 +17,40 @@ public class Demo {
             }
         }
         return winner;
+    }
+
+    private static Painter findCheapest2(double sqMeters, List<Painter> painters) {
+        return painters.stream()
+                .filter(Painter::isAvailable)
+                .min(Comparator.comparing(painter -> painter.estimateCompensation(sqMeters))).get();
+    }
+
+    /**
+     * A selection aggregation
+     * @param sqMeters
+     * @param painters
+     * @return
+     */
+    private static Painter findCheapest3(double sqMeters, List<Painter> painters) {
+        return painters.stream()
+                .filter(Painter::isAvailable)
+                .reduce((acc, current) -> acc.estimateCompensation(sqMeters)
+                .compareTo(current.estimateCompensation(sqMeters)) <= 0 ? acc : current).get();
+    }
+
+    private static Money getTotalCost(double sqMeters, List<Painter> painters) {
+        return painters.stream()
+                .filter(Painter::isAvailable)
+                .reduce(Money.ZERO, (acc, painter) -> painter.estimateCompensation(sqMeters).add(acc),
+                        Money::add);
+    }
+
+    private static Money getTotalCost1(double sqMeters, List<Painter> painters) {
+        return painters.stream()
+                .filter(Painter::isAvailable)
+                .map(painter -> painter.estimateCompensation(sqMeters))
+                .reduce(Money::add)
+                .orElse(Money.ZERO);
     }
 
     public void run() {
