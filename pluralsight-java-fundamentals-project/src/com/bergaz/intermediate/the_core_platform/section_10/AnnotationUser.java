2e96a86fbdd285245e5847e4d1c14f2955b5b237
hsynercn <huscanercan@gmail.com>
2020-10-14 07:38:34 +0300

    Annotation sample

---
+++ pluralsight-java-fundamentals-project/src/com/bergaz/intermediate/the_core_platform/section_10/AnnotationUser.java
@@ -0,0 +1,10 @@
+package com.bergaz.intermediate.the_core_platform.section_10;
+
+@WorkHandler(useThreadPool = false)
+public class AnnotationUser implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ pluralsight-java-fundamentals-project/src/com/bergaz/intermediate/the_core_platform/section_10/WorkHandler.java
@@ -0,0 +1,7 @@
+package com.bergaz.intermediate.the_core_platform.section_10;
+
+public @interface WorkHandler {
+
+    boolean useThreadPool();
+
+}
